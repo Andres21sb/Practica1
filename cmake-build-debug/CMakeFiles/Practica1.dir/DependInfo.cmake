1138bc20f08b3b665b5feefbd078c5e1b7ba4856
Andres21sb <85252206+Andres21sb@users.noreply.github.com>
2022-08-15 13:08:44 -0600

    .
    .

---
+++ cmake-build-debug/CMakeFiles/Practica1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Serch/CLionProjects/Practica1/Practica1/FileManager.cpp" "CMakeFiles/Practica1.dir/FileManager.cpp.obj" "gcc" "CMakeFiles/Practica1.dir/FileManager.cpp.obj.d"
+  "C:/Users/Serch/CLionProjects/Practica1/Practica1/IteradorLista.cpp" "CMakeFiles/Practica1.dir/IteradorLista.cpp.obj" "gcc" "CMakeFiles/Practica1.dir/IteradorLista.cpp.obj.d"
+  "C:/Users/Serch/CLionProjects/Practica1/Practica1/ListaGenerica.cpp" "CMakeFiles/Practica1.dir/ListaGenerica.cpp.obj" "gcc" "CMakeFiles/Practica1.dir/ListaGenerica.cpp.obj.d"
+  "C:/Users/Serch/CLionProjects/Practica1/Practica1/NodoGenerico.cpp" "CMakeFiles/Practica1.dir/NodoGenerico.cpp.obj" "gcc" "CMakeFiles/Practica1.dir/NodoGenerico.cpp.obj.d"
+  "C:/Users/Serch/CLionProjects/Practica1/Practica1/Trabajador.cpp" "CMakeFiles/Practica1.dir/Trabajador.cpp.obj" "gcc" "CMakeFiles/Practica1.dir/Trabajador.cpp.obj.d"
+  "C:/Users/Serch/CLionProjects/Practica1/Practica1/main.cpp" "CMakeFiles/Practica1.dir/main.cpp.obj" "gcc" "CMakeFiles/Practica1.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Practica1.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practica1.dir/FileManager.cpp.obj"
+  "CMakeFiles/Practica1.dir/FileManager.cpp.obj.d"
+  "CMakeFiles/Practica1.dir/IteradorLista.cpp.obj"
+  "CMakeFiles/Practica1.dir/IteradorLista.cpp.obj.d"
+  "CMakeFiles/Practica1.dir/ListaGenerica.cpp.obj"
+  "CMakeFiles/Practica1.dir/ListaGenerica.cpp.obj.d"
+  "CMakeFiles/Practica1.dir/NodoGenerico.cpp.obj"
+  "CMakeFiles/Practica1.dir/NodoGenerico.cpp.obj.d"
+  "CMakeFiles/Practica1.dir/Trabajador.cpp.obj"
+  "CMakeFiles/Practica1.dir/Trabajador.cpp.obj.d"
+  "CMakeFiles/Practica1.dir/main.cpp.obj"
+  "CMakeFiles/Practica1.dir/main.cpp.obj.d"
+  "Practica1.exe"
+  "Practica1.exe.manifest"
+  "Practica1.pdb"
+  "libPractica1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practica1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
