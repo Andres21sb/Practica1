e1c98e7cbac85d2e188e24ba4ca3dde114c7afe7
Andres21sb <85252206+Andres21sb@users.noreply.github.com>
2022-08-15 20:10:26 -0600

    decoraciones
    ajustes main

--- main.cpp
+++ main.cpp
@@ -5,26 +5,9 @@ int main() {
     ListaGenerica<Trabajador>* lista = new ListaGenerica<Trabajador>;
     lista = arch.recuperaTrabajadores();
     lista->ordenaPorPrimerApellidoAscendente();
-    cout<<lista->toString()<<endl;
-   Trabajador* prueba = new Trabajador("118210401","Mendez","Solano","Andres","1",650000);
-   cout<<prueba->deducciones()<<endl;
-   cout<<prueba->salarioNeto()<<endl;
-
-  cout<<"Neto promedio -> "<<lista->promedioSalariosNetos()<<endl;
 
   cout<<"Reporte"<<endl;
   cout<<lista->reporte()<<endl;
-  /*cout<<prueba->toStringFormatoReporte()<<endl;*/
-  prueba = lista->devuelvePorId("736847602");
-  cout<<prueba->toString()<<endl;
-  int tam;
-  tam = prueba->formateaApellidosA20().size();
-  cout<<tam<<endl;
 
-
-
-
-
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
